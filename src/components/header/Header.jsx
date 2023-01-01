456b2de25210f6ffa537e9c5b1a7de07e4a27b6f
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-01-01 17:35:36 +0200

    added Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,45 @@
+import Logo from '../logo/Logo';
+import { Container, Grid, TextField, Button } from '@mui/material';
+import classNames from 'classnames';
+import styles from './Header.module.scss';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+
+export default function Header() {
+  return (
+    <Container className={classNames(styles.header)} maxWidth="xl">
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-around"
+        alignItems="center"
+      >
+        <Grid item xs="auto">
+          <Logo />
+        </Grid>
+        <Grid item xs={4}>
+          <TextField
+            id="search-bar"
+            variant="standard"
+            placeholder="Find items, users and activities"
+            fullWidth
+            sx={{ p: 0.5, borderRadius: 1 }}
+            className={classNames(styles.background_default)}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon className={classNames(styles.primaryText)} />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+        <Grid item xs="auto">
+          <Button variant="text" className={classNames(styles.primaryText)}>Home</Button>
+          <Button variant="text" className={classNames(styles.primaryText)}>Activity</Button>
+          <Button variant="contained">EXPLORE</Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
