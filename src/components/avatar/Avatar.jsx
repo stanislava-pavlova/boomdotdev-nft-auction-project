87711e9d3ba4b47f6a89f1ae1be212fc89dcd0ba
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-01-01 22:29:19 +0200

    added Avatar component

---
+++ src/components/avatar/Avatar.jsx
@@ -0,0 +1,25 @@
+import classNames from 'classnames';
+import styles from './Avatar.module.scss';
+
+export default function Avatar({ size, url, verified }) {
+  return (
+    <div
+      style={{ height: size, width: size }}
+      className={classNames(styles['avatar'])}
+    >
+      <img src={url} alt="avatar" className={classNames(styles['image'])} />
+      <img
+        src={'images/verified.svg'}
+        alt="verified"
+        style={{ display: verified ? 'block' : 'none' }}
+        className={classNames(styles['badge'])}
+      />
+    </div>
+  );
+}
+
+Avatar.defaultProps = {
+  size: 90,
+  url: 'images/avatar.png',
+  verified: false,
+};
