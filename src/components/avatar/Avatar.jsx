c55be2c610e217ee39d3421ca6672df872f3f032
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-01-01 22:44:57 +0200

    fix

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,7 +1,7 @@
 import classNames from 'classnames';
 import styles from './Avatar.module.scss';
 
-export default function Avatar({ size, url, verified }) {
+export default function Avatar({ size = 90, url = 'images/avatar.png', verified = false }) {
   return (
     <div
       style={{ height: size, width: size }}
@@ -18,8 +18,27 @@ export default function Avatar({ size, url, verified }) {
   );
 }
 
-Avatar.defaultProps = {
-  size: 90,
-  url: 'images/avatar.png',
-  verified: false,
-};
+// WORKS:
+
+// export default function Avatar({ size, url, verified }) {
+//   return (
+//     <div
+//       style={{ height: size, width: size }}
+//       className={classNames(styles['avatar'])}
+//     >
+//       <img src={url} alt="avatar" className={classNames(styles['image'])} />
+//       <img
+//         src={'images/verified.svg'}
+//         alt="verified"
+//         style={{ display: verified ? 'block' : 'none' }}
+//         className={classNames(styles['badge'])}
+//       />
+//     </div>
+//   );
+// }
+
+// Avatar.defaultProps = {
+//   size: 90,
+//   url: 'images/avatar.png',
+//   verified: false,
+// };
