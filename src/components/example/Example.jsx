b25636bb745889dfaa09d286e3ac74430b4d6eff
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-01-01 17:35:57 +0200

    fix

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -3,7 +3,6 @@ import Container from '@mui/material/Container';
 import Link from '../link/Link';
 import styles from './Example.module.scss';
 import classNames from 'classnames';
-import Logo from '../logo/Logo';
 
 export default function Copyright() {
   return (
@@ -13,7 +12,6 @@ export default function Copyright() {
           <Link href="/about" color="secondary">
             Hi
           </Link>
-          <Logo />
         </Paper>
       </Container>
     </div>
