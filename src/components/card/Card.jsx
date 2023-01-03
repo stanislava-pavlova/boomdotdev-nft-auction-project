2358113690e7c20dc96f1f16052577113bcf3ee3
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-01-03 11:06:45 +0200

    added Card component

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,71 @@
+import millify from 'millify';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import styles from './Card.module.scss';
+
+import CardContainer from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+// import Avatar from '@mui/material/Avatar';
+import Avatar from '../avatar/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Chip from '@mui/material/Chip';
+import Container from '@mui/material/Container';
+
+export default function Card({
+  name = 'Clock',
+  likes = 1100,
+  mediaUrl,
+  user,
+  price = '~12.2',
+  currency = 'ETH',
+}) {
+  return (
+    <CardContainer sx={{ maxWidth: 345 }}>
+      <CardHeader avatar={<Avatar size={50} verified={true} />} />
+      <CardMedia
+        component="img"
+        height="194"
+        image="images/nft.jpg"
+        alt="image"
+      />
+      <CardActions>
+        <Container className={classNames(styles['cardActions-container'])}>
+          <Typography className={classNames(styles['name'])}>
+            {name}
+          </Typography>
+          <Typography className={classNames(styles['price'])} color="secondary">
+            {price + ' ' + currency}
+          </Typography>
+        </Container>
+        <IconButton aria-label="add to favorites">
+          <Chip
+            className={classNames(styles['likes'])}
+            icon={<FavoriteIcon />}
+            label={millify(likes)}
+            color="secondary"
+            variant="outlined"
+          />
+        </IconButton>
+      </CardActions>
+    </CardContainer>
+  );
+}
+
+Card.propTypes = {
+  name: PropTypes.string,
+  likes: PropTypes.number,
+  mediaUrl: PropTypes.string,
+  user: {
+    avatar: {
+      url: PropTypes.string,
+    },
+    verified: PropTypes.bool,
+  },
+  price: PropTypes.string,
+  currency: PropTypes.string,
+};
