29bce0a6fac2b2d423c3e3bf7ac663ddda86ce93
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-01-07 20:26:04 +0200

    added Trending component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,12 @@
 // import Example from "../src/components/example/Example";
 import Header from '../src/components/header/Header';
+import Trending from '../src/components/trending/Trending';
 
 export default function Index() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Trending />
+    </>
+  );
 }
---
+++ src/components/trending/Trending.jsx
@@ -0,0 +1,91 @@
+import Card from '../card/Card';
+import styles from './Trending.module.scss';
+import classNames from 'classnames';
+
+import { Container, Grid, InputLabel, FormControl, Select, MenuItem  } from '@mui/material';
+
+export default function Trending({ cards = [] }) {
+  return (
+    <Container maxWidth="xl">
+      <header className={classNames(styles.header)}>
+        <h1>Trending</h1>
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+          <InputLabel>This week</InputLabel>
+          <Select id="trending-select-small">
+            <MenuItem>This week</MenuItem>
+            <MenuItem>This month</MenuItem>
+            <MenuItem>This year</MenuItem>
+          </Select>
+        </FormControl>
+      </header>
+      <Grid sx={{ flexGrow: 1 }} container>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={4}>
+            {cards.map((card, value) => (
+              <Grid key={value} item>
+                <Card
+                  name={card.name}
+                  user={card.user}
+                  mediaUrl={card.mediaUrl}
+                  price={card.price}
+                  currency={card.currency}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
+// const cards = [
+//   {
+//     name: 'Ivy',
+//     user: {
+//       avatar: {
+//         url: 'images/avatar.png',
+//       },
+//       verified: true,
+//     },
+//     mediaUrl: 'images/nft.jpg',
+//     price: 1,
+//     currency: 'ETH',
+//   },
+//   {
+//     name: 'Judie',
+//     user: {
+//       avatar: {
+//         url: 'images/avatar.png',
+//       },
+//       verified: true,
+//     },
+//     mediaUrl: 'images/nft.jpg',
+//     price: 2.3,
+//     currency: 'ETH',
+//   },
+//   {
+//     name: 'Juniper',
+//     user: {
+//       avatar: {
+//         url: 'images/avatar.png',
+//       },
+//       verified: true,
+//     },
+//     mediaUrl: 'images/nft.jpg',
+//     price: 5,
+//     currency: 'ETH',
+//   },
+//   {
+//     name: 'Maple',
+//     user: {
+//       avatar: {
+//         url: 'images/avatar.png',
+//       },
+//       verified: true,
+//     },
+//     mediaUrl: 'images/nft.jpg',
+//     price: 10,
+//     currency: 'ETH',
+//   },
+// ];
