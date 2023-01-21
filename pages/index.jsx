d6eb8f8464ec1f89293cd6797c96c702f275778a
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-01-21 18:02:05 +0200

    added Auctions component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
 // import Example from "../src/components/example/Example";
 import Header from '../src/components/header/Header';
 import Trending from '../src/components/trending/Trending';
+import Auctions from '../src/components/auctions/Auctions';
 
 export default function Index() {
   return (
     <>
       <Header />
       <Trending />
+      <Auctions />
     </>
   );
 }
