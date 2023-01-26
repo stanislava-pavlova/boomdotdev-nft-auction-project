b7904e6ffde0ecc2041a400526261988830035b0
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-01-26 18:18:35 +0200

    copy

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,25 +1,54 @@
-import Logo from '../logo/Logo';
 import styles from './Footer.module.scss';
-import { Container, Grid, Typography, Button } from '@mui/material';
-import classNames from 'classnames';
+import {Container, Grid, Typography, Button} from '@mui/material';
+import Logo from '../logo/Logo';
+import classNames from "classnames";
 
 export default function Footer() {
-  return (
-    <Container className={classNames(styles['footer'])} maxWidth="xl">
-      <Grid container justifyContent="space-around" alignItems="center">
-        <Grid item xs="auto">
-          <Logo type="muted" />
-        </Grid>
-        <Grid item xs="auto">
-          <Typography className={classNames(styles['paragraph'])}>
-            Bum All Rights Reserved 2021
-          </Typography>
-        </Grid>
-        <Grid item xs="auto" className={classNames(styles['buttons'])}>
-          <Button>Privacy Policy</Button>
-          <Button>Cookie Policy</Button>
-        </Grid>
-      </Grid>
-    </Container>
-  );
+    return (
+        <Container maxWidth={false} className={classNames(styles['footer'])}>
+            <Grid container justifyContent="space-around" alignItems="center">
+               <Grid item xs="auto">
+                   <Logo type="muted" />
+               </Grid>
+               <Grid item xs="auto">
+                   <Typography className={classNames(styles['paragraph'])} >
+                     Bum All Rights Reserved 2021
+                   </Typography>
+               </Grid>
+               <Grid item xs="auto" className={classNames(styles['buttons'])}>
+                  <Button size="small" variant="text">Privacy Policy</Button>
+                  <Button size="small" variant="text">Cookie Policy</Button>
+               </Grid>
+            </Grid>
+        </Container>
+    )
 }
+
+
+// import Logo from '../logo/Logo';
+// import styles from './Footer.module.scss';
+// import { Container, Grid, Typography, Button } from '@mui/material';
+// import classNames from 'classnames';
+
+// export default function Footer() {
+//   return (
+//     <Container className={classNames(styles['footer'])} maxWidth="xl">
+//       <Grid container justifyContent="space-around" alignItems="center">
+//         <Grid item xs="auto">
+//           <Logo type="muted" />
+//         </Grid>
+//         <Grid item xs="auto">
+//           <Typography className={classNames(styles['paragraph'])}>
+//             Bum All Rights Reserved 2021
+//           </Typography>
+//         </Grid>
+//         <Grid item xs="auto" className={classNames(styles['buttons'])}>
+//           <Grid item>
+//           <Button>Privacy Policy</Button>
+//           </Grid>
+//           <Button>Cookie Policy</Button>
+//         </Grid>
+//       </Grid>
+//     </Container>
+//   );
+// }
