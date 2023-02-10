574ec1338f5c72af4d9ea65967733589aedf9b7b
Stanislava Pavlova <stanislavap2002@gmail.com>
2023-02-10 17:57:42 +0200

    added components

--- src/components/how/How.jsx
+++ src/components/how/How.jsx
@@ -3,56 +3,10 @@ import styles from './How.module.scss';
 import classNames from 'classnames';
 import { Container, Grid, Typography, Button } from '@mui/material';
 
-export default function ({ description, title, items = [], link }) {
+export default function () {
   return (
     <Container className={classNames(styles.container)} maxWidth="xl">
-      <Grid
-        container
-        direction="row"
-        justifyContent="space-around"
-        alignItems="center"
-        className={classNames(styles['how-grid-container'])}
-      >
-        <Grid item>
-          <Typography
-            mb={2}
-            className={classNames(styles['how-heading'])}
-            variant="h1"
-          >
-            {title}
-          </Typography>
-          <Typography
-            mb={4}
-            className={classNames(styles['how-text'])}
-            variant="body1"
-          >
-            {description}
-          </Typography>
-          <Button href={link} variant="contained">
-            learn more
-          </Button>
-        </Grid>
-        <Grid
-          item
-          xs={5}
-          container
-          direction="row"
-          spacing={2}
-          sx={{ position: 'relative', top: '-5%' }}
-        >
-          {items.map((i, index) => {
-            return (
-              <Grid item key={index}>
-                <Step
-                  title={i.title}
-                  description={i.description}
-                  number={i.number}
-                />
-              </Grid>
-            );
-          })}
-        </Grid>
-      </Grid>
+      <Step />
     </Container>
   );
 }
--- src/components/how/Step.jsx
+++ src/components/how/Step.jsx
@@ -3,19 +3,26 @@ import styles from './Step.module.scss';
 import { Typography } from '@mui/material';
 import { Card as CardContainer, CardContent } from '@mui/material';
 
-export default function Step({number, title, description}) {
+export default function Step() {
   return (
-    <CardContainer sx={{ maxWidth: 450, maxHeight: 345, display: 'flex', alignItems: 'center' }}>
+    <CardContainer
+      sx={{
+        maxWidth: 450,
+        maxHeight: 345,
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
       <CardContent className={classNames(styles['number-container'])}>
-        <Typography className={classNames(styles['number'])}>{number}</Typography>
+        <Typography className={classNames(styles['number'])}>1</Typography>
       </CardContent>
 
       <CardContent className={classNames(styles['text-container'])}>
-        <Typography className={classNames(styles['title'])}>
-          {title}
-        </Typography>
+        <Typography className={classNames(styles['title'])}>Title</Typography>
         <Typography className={classNames(styles['description'])}>
-          {description}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
         </Typography>
       </CardContent>
     </CardContainer>
